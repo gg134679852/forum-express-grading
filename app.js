1a68ae7578e6322f813ef843e7f58c742ae14379
gg134679852 <metal852794613@gmail.com>
2021-04-01 22:56:55 +0800

    fix:port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
