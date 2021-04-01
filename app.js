3008c43765e0a41f9d0ec5197ca21f6b60bd796a
gg134679852 <metal852794613@gmail.com>
2021-04-01 20:54:35 +0800

    Install pg and update production config

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const passport = require('./config/passport')
+const db = require('./models')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars()) 
-app.set('view engine', 'handlebars') 
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use('/upload', express.static(__dirname + '/upload'))
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
