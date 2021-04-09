d51b5c4314b49dc04de818b78145df56a764e868
gg134679852 <metal852794613@gmail.com>
2021-04-09 12:00:04 +0800

    feat:feed fix:profile

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
 const helpers = require('./_helpers')
+var MemoryStore = require('memorystore')(session)
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -22,7 +23,16 @@ app.engine('handlebars', handlebars({
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(session({
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000
+  }),
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+// app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use('/upload', express.static(__dirname + '/upload'))
 
 app.use(passport.initialize())
--- controllers/restController.js
+++ controllers/restController.js
@@ -52,7 +52,7 @@ const restController = {
     })
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, {
+    Restaurant.findByPk(req.params.id, {
       include: [
         Category,
         { model: Comment, include: [User] }
@@ -63,29 +63,29 @@ const restController = {
       })
     })
   },
-  // getFeeds: (req, res) => {
-  //   return Promise.all([
-  //     Restaurant.findAll({
-  //       limit: 10,
-  //       raw: true,
-  //       nest: true,
-  //       order: [['createdAt', 'DESC']],
-  //       include: [Category]
-  //     }),
-  //     Comment.findAll({
-  //       limit: 10,
-  //       raw: true,
-  //       nest: true,
-  //       order: [['createdAt', 'DESC']],
-  //       include: [User, Restaurant]
-  //     })
-  //   ]).then(([restaurants, comments]) => {
-  //     return res.render('feeds', {
-  //       restaurants: restaurants,
-  //       comments: comments
-  //     })
-  //   })
-  // }
+  getFeeds: (req, res) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        raw: true,
+        nest: true,
+        order: [['createdAt', 'DESC']],
+        include: [Category]
+      }),
+      Comment.findAll({
+        limit: 10,
+        raw: true,
+        nest: true,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant]
+      })
+    ]).then(([restaurants, comments]) => {
+      return res.render('feeds', {
+        restaurants: restaurants,
+        comments: comments
+      })
+    })
+  }
 }
 
 module.exports = restController
